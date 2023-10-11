91ab9ff2d399882f7523a9bf657bad59af38b6bf
SELAMAWITMEKONNEN <kidestanchinesh@gmail.com>
2023-10-10 22:29:57 -0400

    wip

--- scripts/mvp.sql
+++ scripts/mvp.sql
@@ -1,5 +1,16 @@
 1. 
-    a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
+  --  a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
+ans	  
+	SELECT npi, sum(total_claim_count) AS total_drugs
+	FROM prescriber
+	INNER JOIN prescription
+	USING(npi)
+	GROUP BY (npi)
+	order by total_drugs DESC;
+	
+	ANS 99707
+	
+	
     
     b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
 
